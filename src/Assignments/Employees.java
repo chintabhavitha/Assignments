a607fae98841f927cbc9a7a716a7035bd990094a
Chinta Bhavitha <chintabhavitha@gmail.com>
2025-04-11 14:24:25 +0530

    Assignment3

---
+++ src/Assignments/Employees.java
@@ -0,0 +1,17 @@
+package Assignments;
+
+public class Employees {
+
+	String empname1 = "Bharath";
+	String empname2 = "ABC";
+	String empname3 = "XYZ";
+	int empid1 = 1234;
+	int empid2 = 1235;
+	int empid3 = 1236;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/languages/basics/ClassAndObject3.java
@@ -0,0 +1,13 @@
+package languages.basics;
+
+public class ClassAndObject3 extends ClassAndObject1{
+	
+	
+
+	public static void main(String[] args) {
+		ClassAndObject3 obj= new ClassAndObject3();
+		System.out.println(obj.name);
+
+	}
+
+}
