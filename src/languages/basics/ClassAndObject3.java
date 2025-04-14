054362216124ceec594e103f10dc9f248ed1f394
Chinta Bhavitha <chintabhavitha@gmail.com>
2025-04-14 11:13:21 +0530

    Assignment4

--- src/languages/basics/ClassAndObject3.java
+++ src/languages/basics/ClassAndObject3.java
@@ -5,7 +5,7 @@ public class ClassAndObject3 extends ClassAndObject1{
 	
 
 	public static void main(String[] args) {
-		ClassAndObject3 obj= new ClassAndObject3();
+		ClassAndObject1 obj= new ClassAndObject3(); //ClassAndObject1 or ClassAndObject3 same class or parent class
 		System.out.println(obj.name);
 
 	}
