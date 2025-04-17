fd1d3dba20e2f4ffc232229d54589b67da3a6dba
Chinta Bhavitha <chintabhavitha@gmail.com>
2025-04-17 11:15:31 +0530

    Assignment 6

--- src/Assignments/Assignment6_Operators.java
+++ src/Assignments/Assignment6_Operators.java
@@ -8,6 +8,12 @@ public class Assignment6_Operators {
 		
 		int updatedMarks[] = new int[3];
 		
+//		System.out.println("Updated Marks: ");
+//		System.out.println("Suresh :" + (marks[0] + 10));
+//		System.out.println("Mahesh : " + (marks[1]+10));
+//		System.out.println("Naresh : " + (marks[2]+10));
+		
+		
 		updatedMarks[0] = marks[0]+ 10;
 		updatedMarks[1] = marks[1]+ 10;
 		updatedMarks[2] = marks[2]+ 10;
