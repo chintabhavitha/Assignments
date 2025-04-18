138d786a494b84c254468f536e9dc111b512a4e5
Chinta Bhavitha <chintabhavitha@gmail.com>
2025-04-18 17:00:49 +0530

    Assignment 7

--- src/Assignments/Assignment7_ConditionalStatements.java
+++ src/Assignments/Assignment7_ConditionalStatements.java
@@ -13,15 +13,15 @@ public class Assignment7_ConditionalStatements {
 			System.out.println("The loan is automatically approved");
 
 		} else if (creditScore >= 650 && creditScore <= 750) {
-			System.out.println("Additional Checks are Performed because credit score is 720");
+			//System.out.println("Additional Checks are Performed because credit score is 720");
 
-			if (creditScore <= 750 && customerIncome <= 50000) {
-				System.out.println("The loan to be considered becuase employee income is at least 50000");
+			if (customerIncome >= 50000) {
+				//System.out.println("The loan to be considered because employee income is at least 50000");
 
 				if (isEmployed) {
-					System.out.println("The customer is employed and loan is considered");
+					//System.out.println("The customer is employed and loan is considered");
 
-					if (debtToIncomeRatio < 40) {
+					if (debtToIncomeRatio < 40.0) {
 						System.out.println("The loan is Approved");
 					} else {
 						System.out.println("The loan is denied due to high debt-to-income ratio");
