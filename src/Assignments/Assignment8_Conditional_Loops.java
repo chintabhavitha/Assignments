d63b1985ba95d063e21f619babb27d10f7603922
Chinta Bhavitha <chintabhavitha@gmail.com>
2025-04-30 11:45:20 +0530

    Assignment 10

--- src/Assignments/Assignment8_Conditional_Loops.java
+++ src/Assignments/Assignment8_Conditional_Loops.java
@@ -17,7 +17,7 @@ public class Assignment8_Conditional_Loops {
 		transaction.add(-300);
 		transaction.add(-3000);
 
-		System.out.println("1. The total Number of transactions are " + transaction.size());
+		System.out.println("The total Number of transactions are " + transaction.size());
 //
 //		int sumOfCredit = 0;
 //		for (Integer i : transaction) {
@@ -34,36 +34,47 @@ public class Assignment8_Conditional_Loops {
 //			}
 //		}
 //		System.out.println("2. The total amount of debited transaction is " + sumOfDebit);
-		
-		int sumOfCredit = 0; 
+
+		int sumOfCredit = 0;
 		int sumOfDebit = 0;
-		
-		for(Integer i:transaction) {
-			if(i>0) {
+		int totalCredits = 0;
+		int totalDebits = 0;
+		int suspiciousTransactions = 0;
+
+		for (Integer i : transaction) {
+			if (i > 0) {
+				totalCredits++;
 				sumOfCredit += i;
-			}
-			else {
+
+				if (i > 10000) {
+					System.out.println("Suspicious credit transaction " + i);
+					suspiciousTransactions++;
+				}
+			} else {
+				totalDebits++;
 				sumOfDebit += i;
-			}
+
+				if (i < -10000) {
+					System.out.println("Suspicious debit transaction " + i);
+					suspiciousTransactions++;
+
+				}
 //			if(i>10000 || i<-10000) {
 //				System.out.println("4. Suspicious credit/ debit Transaction with Amount are " + i);
 //			}
-		}
-		System.out.println("2. The total amount of credited transaction is " + sumOfCredit + "\n" + "   The total amount of debited transaction is  " + sumOfDebit );
-		
-		int remaining = sumOfCredit + sumOfDebit;
-		System.out.println("3. The total amount remaining at the end in Bank Account is " + remaining);
-
-		for (Integer k : transaction) {
-			if (k > 10000 || k < -10000) {
-				System.out.println("4. Suspicious credit/ debit Transaction with Amount are " + k);
-
 			}
 		}
+		int remaining = sumOfCredit + sumOfDebit;
+		System.out.println("Total no. of credits are " + totalCredits);
+		System.out.println("Total no. of debits are " + totalDebits);
+		System.out.println("The total amount remaining at the end in Bank Account is " + remaining);
+		System.out.println(
+				"The total credited amount is " + sumOfCredit + "\n" + "The total debited amount is  " + sumOfDebit);
+		System.out.println("Total Suspicious transactions are " + suspiciousTransactions);
 
-		
+//		for (Integer k : transaction) {
+//			if (k > 10000 || k < -10000) {
+//				System.out.println("4. Suspicious credit/ debit Transaction with Amount are " + k);
 
-		
 	}
-
 }
